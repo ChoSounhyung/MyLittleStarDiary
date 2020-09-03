1bffb9847940c49d766fed740b27e14b0aa4733e
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-03 21:43:26 +0900

    일기 화면 nav 동작 구현

---
+++ app/src/main/java/com/mirinae/mylittlestardiary/DiaryActivity.java
@@ -0,0 +1,45 @@
+package com.mirinae.mylittlestardiary;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.diary);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.star:
+                        startActivity(new Intent(getApplicationContext(),StarActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.diary:
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
