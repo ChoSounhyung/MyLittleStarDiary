426c677f435c3f845b61f9a4f594e99072f551d7
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-03 21:42:55 +0900

    메인 화면 nav 동작 구현

--- app/src/main/java/com/mirinae/mylittlestardiary/MainActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/MainActivity.java
@@ -1,8 +1,13 @@
 package com.mirinae.mylittlestardiary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        return true;
+                    case R.id.star:
+                        startActivity(new Intent(getApplicationContext(),StarActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.diary:
+                        startActivity(new Intent(getApplicationContext(),DiaryActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
