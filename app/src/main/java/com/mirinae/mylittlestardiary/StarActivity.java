18c709921e0708571c0d7a7f00a94b870b8e3de1
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-05 20:13:05 +0900

    star, detail, back intent 연결

--- app/src/main/java/com/mirinae/mylittlestardiary/StarActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/StarActivity.java
@@ -6,15 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class StarActivity extends AppCompatActivity {
+    private ImageButton lionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star);
+
+        init();
+        setUp();
 
         //Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
@@ -41,5 +47,23 @@ public class StarActivity extends AppCompatActivity {
                 return false;
             }
         });
+
     }
+
+    private void init() {
+        lionBtn = findViewById(R.id.star_lion_btn);
+    }
+
+    private void setUp() {
+        lionBtn.setOnClickListener(goStarDetailPage);
+    }
+
+    // Image Button Click
+    View.OnClickListener goStarDetailPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(StarActivity.this, StarDetailActivity.class);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/com/mirinae/mylittlestardiary/StarDetailActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/StarDetailActivity.java
@@ -1,13 +1,30 @@
 package com.mirinae.mylittlestardiary;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class StarDetailActivity extends AppCompatActivity {
+    private ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star_detail);
+
+        backBtn = findViewById(R.id.star_back);
+        backBtn.setOnClickListener(goBackPage);
     }
+
+    View.OnClickListener goBackPage = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            StarDetailActivity.this.finish();
+        }
+    };
+
 }
