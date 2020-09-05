f74b761574a852580cb1b5c8fb26a5b0b543fb8c
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-05 20:09:46 +0900

    detail 페이지 toolbar rect  추가

---
+++ app/src/main/java/com/mirinae/mylittlestardiary/StarDetailActivity.java
@@ -0,0 +1,13 @@
+package com.mirinae.mylittlestardiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StarDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star_detail);
+    }
+}
