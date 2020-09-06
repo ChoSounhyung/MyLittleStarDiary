b7d7bf40c8cfdff2326ed96de6cc44f5ecaa3d28
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-06 23:02:45 +0900

    일기 페이지 back버튼 클릭 이벤트 처리

--- app/src/main/java/com/mirinae/mylittlestardiary/DiaryActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/DiaryActivity.java
@@ -3,21 +3,45 @@ package com.mirinae.mylittlestardiary;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import static com.mirinae.mylittlestardiary.R.id.star_back;
+
 public class DiaryActivity extends AppCompatActivity {
+    private ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
+        init();
+        setUp();
         setBottomNav();
     }
+
+    private void init() {
+        backBtn = findViewById(R.id.diary_back);
+    }
+
+    private void setUp() {
+        backBtn.setOnClickListener(goBackPage);
+    }
+
+    View.OnClickListener goBackPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            DiaryActivity.this.finish();
+        }
+    };
 
     public void setBottomNav() {
         //Initialize and assign variable
@@ -46,4 +70,8 @@ public class DiaryActivity extends AppCompatActivity {
             }
         });
     }
+
+//    View.OnClickListener = new View.OnClickListener() {
+//
+//    }
 }
