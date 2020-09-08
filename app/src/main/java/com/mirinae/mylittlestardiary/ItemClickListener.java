04323ee1eff32cb99b23922407d2581927760a18
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-08 23:51:27 +0900

    detail 페이지로 이동하는 onclicklistener 구현

--- app/src/main/java/com/mirinae/mylittlestardiary/ItemClickListener.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/ItemClickListener.java
@@ -1,4 +1,24 @@
 package com.mirinae.mylittlestardiary;
 
-public class ItemClickListener {
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+
+public class ItemClickListener implements View.OnClickListener {
+    private Activity activity;
+    private int itemId;
+
+    public ItemClickListener(Activity activity, int itemId) {
+        this.activity = activity;
+        this.itemId = itemId;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(activity, StarDetailActivity.class);
+
+        intent.putExtra("itemId", itemId);
+
+        activity.startActivity(intent);
+    }
 }
