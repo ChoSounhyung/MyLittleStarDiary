58e5d4f50cb72b311aeb6a5ef9c8ce57dd87b952
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-08 23:59:02 +0900

    diary recyclerview adapter 생성

--- app/src/main/java/com/mirinae/mylittlestardiary/adapter/DiaryItemAdapter.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/adapter/DiaryItemAdapter.java
@@ -11,45 +11,49 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mirinae.mylittlestardiary.Diary;
+import com.mirinae.mylittlestardiary.ItemClickListener;
+import com.mirinae.mylittlestardiary.R;
 
 import java.util.List;
 
-public class DiaryItemAdapter extends RecyclerView.Adapter<DiaryItemAdapter.SearchItemViewHolder> {
-    private List<Diary> diaryList;
-    private Activity searchActivity;
+import static com.mirinae.mylittlestardiary.adapter.DiaryItemAdapter.*;
 
-    public static class SearchItemViewHolder extends RecyclerView.ViewHolder {
-        LinearLayout searchItemContainer;
-        TextView searchTitle;
+public class DiaryItemAdapter extends RecyclerView.Adapter<DiaryItemViewHolder> {
+    private List<Diary> diaryList;
+    private Activity mainActivity;
+
+    public static class DiaryItemViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout diaryItemContainer;
+        TextView diaryTitle;
 
         // Constructor
-        public SearchItemViewHolder(View v) {
+        public DiaryItemViewHolder(View v) {
             super(v);
-            searchItemContainer = (LinearLayout) v.findViewById(R.id.search_item_container);
-            searchTitle = (TextView) v.findViewById(R.id.search_title);
+            diaryItemContainer = (LinearLayout) v.findViewById(R.id.diary_item_container);
+            diaryTitle = (TextView) v.findViewById(R.id.diary_title);
         }
     }
 
     // Constructor
-    public SearchItemAdapter(List<Cosmetic> cosmetics, Activity searchActivity) {
-        this.cosmetics = cosmetics;
-        this.searchActivity = searchActivity;
+    public DiaryItemAdapter(List<Diary> diaryList, Activity mainActivity) {
+        this.diaryList = diaryList;
+        this.mainActivity = mainActivity;
     }
 
     @NonNull
     @Override
-    public SearchItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.search_item, viewGroup, false);
-        return new SearchItemViewHolder(view);
+    public DiaryItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.diary_item, viewGroup, false);
+        return new DiaryItemViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull SearchItemViewHolder holder, int position) {// onClick event
-        holder.searchItemContainer.setOnClickListener(new ItemClickListener(searchActivity, 1));
+    public void onBindViewHolder(@NonNull DiaryItemViewHolder holder, int position) {// onClick event
+        holder.diaryItemContainer.setOnClickListener(new ItemClickListener(mainActivity, 1));
     }
 
     @Override
     public int getItemCount() {
-        return cosmetics.size();
+        return diaryList.size();
     }
 }
