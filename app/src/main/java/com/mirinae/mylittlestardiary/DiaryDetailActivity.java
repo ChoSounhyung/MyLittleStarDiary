e5f74d8ba0c7d7c7b1b9b01679ae9bc2ee5b1d48
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-10 00:10:03 +0900

    add nav 없앰

---
+++ app/src/main/java/com/mirinae/mylittlestardiary/DiaryDetailActivity.java
@@ -0,0 +1,13 @@
+package com.mirinae.mylittlestardiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiaryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_detail);
+    }
+}
