9a2030961f61c71efc18368cc1e401e1d0a81d7e
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-10 00:18:41 +0900

    back btn 클릭 리스너 연결 실패

--- app/src/main/java/com/mirinae/mylittlestardiary/DiaryDetailActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/DiaryDetailActivity.java
@@ -1,13 +1,40 @@
 package com.mirinae.mylittlestardiary;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class DiaryDetailActivity extends AppCompatActivity {
+
+    private ImageButton backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_detail);
+
+        init();
+        Log.d("태그", "정상적으로 init됨");
+        setUp();
+        Log.d("태그", "정상적으로 setup됨");
     }
+
+    private void init() {
+        backBtn = findViewById(R.id.diary_back);
+    }
+
+    private void setUp() {
+        backBtn.setOnClickListener(goBackPage);
+    }
+
+    View.OnClickListener goBackPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            DiaryDetailActivity.this.finish();
+        }
+    };
 }
