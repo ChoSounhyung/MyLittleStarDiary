8923c1475907cfb490c67771245e52691774501f
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-10 22:54:03 +0900

    backBtn intent 연결

--- app/src/main/java/com/mirinae/mylittlestardiary/AddActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/AddActivity.java
@@ -1,14 +1,42 @@
 package com.mirinae.mylittlestardiary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AddActivity extends AppCompatActivity {
+    private ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        init();
+        setUp();
+
     }
+
+    private void init() {
+        backBtn = findViewById(R.id.add_back);
+    }
+
+    private void setUp() {
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddActivity.this.finish();
+            }
+        });
+    }
+
+
 }
