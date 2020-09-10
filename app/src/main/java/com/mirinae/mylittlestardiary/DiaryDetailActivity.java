d1e287c67575f31b45ae52ccc5acefd84b3cd4e1
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-10 22:55:24 +0900

    intent 연결

--- app/src/main/java/com/mirinae/mylittlestardiary/DiaryDetailActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/DiaryDetailActivity.java
@@ -5,12 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class DiaryDetailActivity extends AppCompatActivity {
 
-    private ImageButton backBtn;
+    private ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +17,11 @@ public class DiaryDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_diary_detail);
 
         init();
-        Log.d("태그", "정상적으로 init됨");
         setUp();
-        Log.d("태그", "정상적으로 setup됨");
     }
 
     private void init() {
-        backBtn = findViewById(R.id.diary_back);
+        backBtn = (ImageView) findViewById(R.id.diary_back);
     }
 
     private void setUp() {
