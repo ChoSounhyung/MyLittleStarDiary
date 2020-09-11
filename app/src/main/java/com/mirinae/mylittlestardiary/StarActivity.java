58beae22f74a5c6e73e9bcac5012a6fbe3e48b4a
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-12 00:48:11 +0900

    설화 화면 intent 연결 실패

--- app/src/main/java/com/mirinae/mylittlestardiary/StarActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/StarActivity.java
@@ -4,23 +4,62 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.ByteArrayOutputStream;
+
 public class StarActivity extends AppCompatActivity {
+    public static final String EXTRA_STAR = "com.mirinae.mylittlestardiary.EXTRA_STAR";
+    public static final String EXTRA_DATE = "com.mirinae.mylittlestardiary.EXTRA_DATE";
+    public static final String EXTRA_TEXT = "com.mirinae.mylittlestardiary.EXTRA_TEXT";
+
+    private TextView detailDate;
+    private TextView detailText;
+
+    private ImageView bottle;
+    private ImageView fish;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star);
 
+        init();
         setBottomNav();
+        setUp();
 
     }
+
+    public void init() {
+        detailDate = findViewById(R.id.star_detail_date);
+        detailText = findViewById(R.id.star_detail_text);
+        bottle = findViewById(R.id.star_bottle_btn);
+        fish = findViewById(R.id.star_fish_btn);
+    }
+
+    public void setUp() {
+        bottle.setOnClickListener(goDetailPage);
+        fish.setOnClickListener(goDetailPage);
+    }
+
+    View.OnClickListener goDetailPage = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 
     public void setBottomNav() {
         //Initialize and assign variable
--- app/src/main/java/com/mirinae/mylittlestardiary/StarDetailActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/StarDetailActivity.java
@@ -1,20 +1,39 @@
 package com.mirinae.mylittlestardiary;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InlineSuggestionsRequest;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class StarDetailActivity extends AppCompatActivity {
     private ImageView bottle;
     private ImageView fish;
 
+    private TextView Detaildate;
+    private TextView Detailtext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star_detail);
+
+        Detaildate = findViewById(R.id.star_detail_date);
+        Detailtext = findViewById(R.id.star_detail_text);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
     }
 }
