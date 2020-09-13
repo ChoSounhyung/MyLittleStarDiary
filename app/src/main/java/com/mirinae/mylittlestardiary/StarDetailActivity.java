4955e309608da7f7c81e2f16b80696b0e51e667d
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-13 22:26:26 +0900

    starActivity에서 넘어온 값 받기

--- app/src/main/java/com/mirinae/mylittlestardiary/StarDetailActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/StarDetailActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InlineSuggestionsRequest;
@@ -16,24 +17,95 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class StarDetailActivity extends AppCompatActivity {
-    private ImageView bottle;
-    private ImageView fish;
+    private ImageView DetailStar;
+    private TextView DetailDate;
+    private TextView DetailText;
 
-    private TextView Detaildate;
-    private TextView Detailtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star_detail);
 
-        Detaildate = findViewById(R.id.star_detail_date);
-        Detailtext = findViewById(R.id.star_detail_text);
+        init();
+        onActivity();
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    private void init() {
+        DetailStar = findViewById(R.id.star_detail_star);
+        DetailDate = findViewById(R.id.star_detail_date);
+        DetailText = findViewById(R.id.star_detail_text);
+    }
 
+    protected void onActivity() {
+        Bundle b = getIntent().getExtras();
+
+        int image_link = (int) b.get("image");
+        String date = (String) b.get(StarActivity.EXTRA_DATE);
+        String text = (String) b.get(StarActivity.EXTRA_TEXT);
+
+        switch (date) {
+            case "1/20~2/18":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "2/19~3/20":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "3/21~4/19":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "4/20~5/20":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "5/21~6/21":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "6/22~7/22":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "7/23~8/22":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "8/23~9/23":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "9/24~10/22":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "10/23~11/22":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "11/23~12/24":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+            case "12/25~1/19":
+                DetailStar.setImageResource(image_link);
+                DetailDate.setText(date);
+                DetailText.setText(text);
+                break;
+        }
 
     }
 }
