e13b6ec89a6f32d91fa8686b672200b8ef2d90c8
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-16 18:53:12 +0900

    Splash 동작 시간, intent 추가

--- app/src/main/java/com/mirinae/mylittlestardiary/SplashActivity.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/SplashActivity.java
@@ -1,4 +1,20 @@
 package com.mirinae.mylittlestardiary;
 
-public class SplashActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+
+        try {
+            Thread.sleep(3000);     // 대기 초 설정
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
 }
