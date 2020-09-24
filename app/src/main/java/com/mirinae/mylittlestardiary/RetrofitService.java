7dd6189922f8c26732dcf998b017989893f3cf53
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-24 23:57:09 +0900

    서버 url 변경

--- app/src/main/java/com/mirinae/mylittlestardiary/RetrofitService.java
+++ app/src/main/java/com/mirinae/mylittlestardiary/RetrofitService.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
@@ -11,7 +12,7 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface RetrofitService {
-    String URL = "http://13.209.41.209:53927";
+    String URL = "http://mirinaediary.emirim.kr:4055";
 
     @GET("/app_diary/diary_list/{diary_id}")
     Call<Diary> getDiary(@Path("diary_id") String diaryId);
@@ -23,4 +24,7 @@ public interface RetrofitService {
     @FormUrlEncoded
     @POST("/app_diary/diary_list")
     Call<Diary> postDiary(@FieldMap HashMap<String, Object> param);
+
+    @POST("/app_diary/diary_list")
+    Call<Void> joinDiaryContent(@Body Diary diary);
 }
